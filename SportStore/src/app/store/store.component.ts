083a7dbc67eec655eb7cf158e9057dee51f7a61d
duda <duda_ebbers@hotmail.com>
2023-09-14 18:05:33 -0300

    product pagination - modif. store html

--- SportStore/src/app/store/store.component.ts
+++ SportStore/src/app/store/store.component.ts
@@ -8,20 +8,43 @@ import { ProductRepository } from "../model/product.repository";
 })
 export class StoreComponent {
     selectedCategory: string | undefined;
+    selectedPage = 1;
+    productsPerPage = 4;
 
     constructor(private repository: ProductRepository) {
          
     }
 
     get products(): Product[] {
-        return this.repository.getProducts(this.selectedCategory);
+        let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+        return this.repository
+            .getProducts(this.selectedCategory)
+            .slice(pageIndex, pageIndex + this.productsPerPage);
     }
 
     get categories(): string[] {
         return this.repository.getCategories();
     }
 
+    get pageNumbers(): number[] {
+        return Array(Math.ceil(this.repository
+            .getProducts(this.selectedCategory).length / this.productsPerPage))
+            .fill(0)
+            .map((x, i) => i + 1);
+    }
+
     changeCategory(newCategory?: string) {
         this.selectedCategory = newCategory;
     }
+
+    changePage(newPage: number) {
+        this.selectedPage = newPage;
+    }
+
+
+    changePageSize(newSize: number) {
+        this.productsPerPage = Number(newSize);
+        this.changePage(1);
+    }
+
 }
