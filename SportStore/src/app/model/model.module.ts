26b2849e6f66912b70ab3bbffabb86346418d042
duda <duda_ebbers@hotmail.com>
2023-09-14 16:00:27 -0300

    model, datasource, repository, modelModule

---
+++ SportStore/src/app/model/model.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { StaticDataSource } from "./static.datasource";
+import { ProductRepository } from "./product.repository";
+
+@NgModule({
+    providers: [
+        StaticDataSource,
+        ProductRepository
+    ]
+})
+export class ModelModule {
+    
+}
