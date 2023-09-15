2b689a0b227498e233753e3e1afa1ea408620e7a
duda <duda_ebbers@hotmail.com>
2023-09-15 12:47:30 -0300

    rest datasource. modif. modelModule

--- SportStore/src/app/model/model.module.ts
+++ SportStore/src/app/model/model.module.ts
@@ -4,14 +4,20 @@ import { ProductRepository } from "./product.repository";
 import { Cart } from "./cart.model";
 import { Order } from "./order.model";
 import { OrderRepository } from "./order.repository";
+import { HttpClientModule } from "@angular/common/http";
+import { RestDataSource } from "./rest.datasource";
 
 @NgModule({
+    imports: [
+        HttpClientModule
+    ],
     providers: [
         StaticDataSource,
         ProductRepository,
         Cart,
         Order,
-        OrderRepository
+        OrderRepository,
+        { provide: StaticDataSource, useClass: RestDataSource }
     ]
 })
 export class ModelModule {
